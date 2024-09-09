2f933c2cc8fa75845befee9a120df29e6ca9b5f7
artforme <me.peremy@gmail.com>
2024-09-09 20:59:37 +0300

    update file

--- cmd/CoordinatesChanger/main.go
+++ cmd/CoordinatesChanger/main.go
@@ -9,10 +9,40 @@ import (
 
 func main() {
 	// Run the main function of the application
+	var tempSystemCoordinates string
+	systemsCoordinatesObject := InitCoordinates(tempSystemCoordinates)
 
-	var (
-		tempSystemCoordinates string
-	)
+	fmt.Println("The coordinates system you have chosen is:", systemsCoordinatesObject.Info())
+	var decimalPlaces uint
+	fmt.Println("Enter the number of decimal places:")
+	fmt.Scan(&decimalPlaces)
+	fmt.Println("The coordinates are:", systemsCoordinatesObject.String(decimalPlaces))
+
+	for {
+		fmt.Println("Enter the coordinates system you want to convert to:")
+		fmt.Println("CARTESIAN type 1, CYLINDRICAL type 2, SPHERICAL type 3, Change the coordinates type 4")
+		fmt.Scan(&tempSystemCoordinates)
+		tempSystemCoordinates = strings.ToUpper(tempSystemCoordinates)
+		switch tempSystemCoordinates {
+		case "1":
+			systemsCoordinatesObject = systemsCoordinatesObject.ConvertIntoCartesian()
+		case "2":
+			systemsCoordinatesObject = systemsCoordinatesObject.ConvertIntoCartesian().ConvertIntoCylindrical()
+		case "3":
+			systemsCoordinatesObject = systemsCoordinatesObject.ConvertIntoCartesian().ConvertIntoSpherical()
+		case "4":
+			systemsCoordinatesObject = InitCoordinates(tempSystemCoordinates)
+		default:
+			fmt.Println("finishing the program")
+			return
+		}
+		fmt.Println("The coordinates system you have chosen is:", systemsCoordinatesObject.Info())
+		fmt.Println("The coordinates are:", systemsCoordinatesObject.String(decimalPlaces))
+	}
+
+}
+
+func InitCoordinates(tempSystemCoordinates string) coordinatesInterface.CoordinatesSystem {
 	fmt.Println("Enter the coordinates system you want to start with:")
 	fmt.Println("CARTESIAN, CYLINDRICAL, SPHERICAL")
 	fmt.Scan(&tempSystemCoordinates)
@@ -23,33 +53,7 @@ func main() {
 
 	if systemsCoordinatesObject == nil {
 		fmt.Println("Invalid coordinates system")
-		return
+		return nil
 	}
-
-	fmt.Println("The coordinates system you have chosen is:", systemsCoordinatesObject.Info())
-	var decimalPlaces uint
-	fmt.Println("Enter the number of decimal places:")
-	fmt.Scan(&decimalPlaces)
-	fmt.Println("The coordinates are:", systemsCoordinatesObject.String(decimalPlaces))
-
-	for {
-		fmt.Println("Enter the coordinates system you want to convert to:")
-		fmt.Println("CARTESIAN type 1, CYLINDRICAL type 2, SPHERICAL type 3")
-		fmt.Scan(&tempSystemCoordinates)
-		tempSystemCoordinates = strings.ToUpper(tempSystemCoordinates)
-		switch tempSystemCoordinates {
-		case "1":
-			systemsCoordinatesObject = systemsCoordinatesObject.ConvertIntoCartesian()
-		case "2":
-			systemsCoordinatesObject = systemsCoordinatesObject.ConvertIntoCartesian().ConvertIntoCylindrical()
-		case "3":
-			systemsCoordinatesObject = systemsCoordinatesObject.ConvertIntoCartesian().ConvertIntoSpherical()
-		default:
-			fmt.Println("finishing the program")
-			return
-		}
-		fmt.Println("The coordinates system you have chosen is:", systemsCoordinatesObject.Info())
-		fmt.Println("The coordinates are:", systemsCoordinatesObject.String(decimalPlaces))
-	}
-
+	return systemsCoordinatesObject
 }
